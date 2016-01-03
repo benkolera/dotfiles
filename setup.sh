a421fe44e5aed38b95c860253c193125335548b6
Ben Kolera <ben.kolera@gmail.com>
2016-01-03 14:17:31 +1000

    xmonad stuff

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 mkdir -p ~/.config
-ln -sf $(pwd)/.zshrc ~/.zshrc
-ln -sf $(pwd)/.zsh.d ~/.zsh.d
-ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
-ln -sf $(pwd)/.config/nvim ~/.config/nvim
-ln -sf $(pwd)/extras/vim-plug/plug.vim ~/.config/nvim/autoload/plug.vim
+ln -sf $(pwd)/.zshrc $HOME/.zshrc
+ln -sf $(pwd)/.zsh.d $HOME/
+ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -sf $(pwd)/.config/nvim $HOME/.config/
+ln -sf $(pwd)/extras/vim-plug/plug.vim $HOME/.config/nvim/autoload/plug.vim
+ln -sf $(pwd)/.xmonad/ $HOME/
 
 echo sh install.$1.sh
