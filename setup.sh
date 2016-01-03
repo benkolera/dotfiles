289555f8c904f37328057923689846a135dbe4bf
Ben Kolera <ben.kolera@gmail.com>
2016-01-03 18:49:43 +1100

    And some other tweaks for nvim

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
 mkdir -p ~/.config
 
@@ -15,12 +15,14 @@ files=(
 
 dirs=(.zsh.d .xmonad .zprezto .config/nvim)
 
-for f in $files; do
+for f in ${files[@]}; do
   ln -sf $(pwd)/$f $HOME/$f
 done;
 
-for d in $dirs; do
+for d in ${dirs[@]}; do
   ln -sf $(pwd)/$d $(dirname $HOME/$d)
 done;
+
+ln -s $(pwd)/extras/vim-plug/plug.vim $HOME/.config/nvim/autoload/plug.vim
 
 sh install.$1.sh
